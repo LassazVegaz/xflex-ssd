2d86927a0ff2ffa7cbc767126e70c52e65937110
LassazVegaz <lasindunuwanga@gmail.com>
2023-09-26 16:49:34 +0530

    java proj init

--- backend/tests/db.test.js
+++
@@ -1,7 +0,0 @@
-import mongoService from "../services/db.service";
-
-describe("Testing db service", () => {
-	it("Test db connection", () => {
-		expect(mongoService.connectDB).not.toThrow();
-	});
-});
